e823e75e9fd3d5107ab68fbab7623779a2939b52
Divon2 <91172013+Divon2@users.noreply.github.com>
2021-10-05 19:45:19 +0700

    Add files via upload
    a = int(input('Masukkan nilai a: '))
    b = int(input('Masukkan nilai b: '))
    c = int(input('Masukkan nilai c: ')) 
    
    if a > b and a > c:
      print('A yang terbesar')
    elif b > a and b > c:
      print('B yang terbesar')
    else:
      print('C yang terbesar') 
      
    a: 256 
    b: 987
    c: 543

---
+++ Angka terbesar.py
@@ -0,0 +1,16 @@
+a = int(input('Masukkan nilai a: '))
+b = int(input('Masukkan nilai b: '))
+c = int(input('Masukkan nilai c: ')) 
+
+if a > b and a > c:
+  print('A yang terbesar')
+elif b > a and b > c:
+  print('B yang terbesar')
+else:
+  print('C yang terbesar') 
+  
+a: 256 
+b: 987
+c: 543
+
+
